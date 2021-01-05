5991513bd35d4891e37303abb44ecdd21c00e58c
Dmitry Babanin <babanin.dima@gmail.com>
2021-01-05 16:34:25 +0300

    feat: Created Employee rest controller

---
+++ src/main/java/by/babanin/springboot/employeeslist/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package by.babanin.springboot.employeeslist.controller;
+
+import by.babanin.springboot.employeeslist.model.Employee;
+import by.babanin.springboot.employeeslist.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+}
